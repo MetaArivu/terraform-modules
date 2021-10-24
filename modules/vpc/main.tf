614b992a7205b04569646ec2b520d692f90f0ebd
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 10:34:35 +0530

    Add files via upload

---
+++ modules/vpc/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "aws-vpc-ariu" {
+    cidr_block = "${var.ariu_aws_vpc_cidr}"
+    enable_dns_support = "true" #gives you an internal domain name
+    enable_dns_hostnames = "true" #gives you an internal host name
+    enable_classiclink = "false"
+    instance_tenancy = "${var.ariu_tenancy}"    
+    tags = {
+        Name = "aws-vpc-ariu"
+}
+}
+
+resource "aws_subnet" "aws-subnet-public-ariu" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "${var.ariu_aws_subnet_cidr}"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "${var.ariu_aws_availability_zone}"
+    tags = {
+        Name = "aws-subnet-public-ariu"
+    }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.aws-vpc-ariu.id}"
+}
