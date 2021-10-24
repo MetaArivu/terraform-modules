897888df570e3bd7f17a834d87c933b9d457dc72
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 10:45:23 +0530

    Create vars.tf

---
+++ modules/rds/vars.tf
@@ -0,0 +1,13 @@
+variable "insname" {
+  type = string  
+}
+
+variable "usr" {
+  type = string  
+}
+variable "pass" {
+  type = string  
+}
+variable "ariu_subnet_id" {
+    default = "subnet-02757ece6403d3678"
+    }
