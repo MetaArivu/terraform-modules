f800b5adf67ec8a5e2adf82932440b61c6d05c18
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 23:15:32 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,30 +1,30 @@
 
 
-resource "aws_vpc" "aws-vpc-ariu" {
-    cidr_block = "${var.ariu_aws_vpc_cidr}"
+resource "aws_vpc" "arivu-aws-vpc" {
+    cidr_block = "${var.arivu_aws_vpc_cidr}"
     enable_dns_support = "true" #gives you an internal domain name
     enable_dns_hostnames = "true" #gives you an internal host name
     enable_classiclink = "false"
-    instance_tenancy = "${var.ariu_tenancy}"    
+    instance_tenancy = "${var.arivu_tenancy}"    
     tags = {
-        Name = "aws-vpc-ariu"
+        Name = "aws-vpc-arivu"
 }
 }
 
-resource "aws_subnet" "aws-subnet-public-ariu" {
+resource "aws_subnet" "aws-subnet-public-arivu" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = "${var.ariu_aws_subnet_cidr}"
+    cidr_block = "${var.arivu_aws_subnet_cidr}"
     map_public_ip_on_launch = "true" //it makes this a public subnet
-    availability_zone = "${var.ariu_aws_availability_zone}"
+    availability_zone = "${var.arivu_aws_availability_zone}"
     tags = {
-        Name = "aws-subnet-public-ariu"
+        Name = "aws-subnet-public-arivu"
     }
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.aws-vpc-ariu.id}"
+  value = "${aws_vpc.aws-vpc-arivu.id}"
 }
 
 output "subnet_id" {
-  value = "${aws_subnet.aws-subnet-public-ariu.id}"
+  value = "${aws_subnet.aws-subnet-public-arivu.id}"
 }
