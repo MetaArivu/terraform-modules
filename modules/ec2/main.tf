712ffe6cebd8661b67159bae6510943c65143591
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 23:13:06 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "aws-ec2-ariu" {
+resource "aws_instance" "arivu-aws-ec2" {
   count         = "${var.ec2_count}"
   ami           = "${lookup(var.AMI, var.AWS_REGION)}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${var.ariu_subnet_id}"
 
   tags = {
-    Name = "aws-ec2-ariu"
+    Name = "arivu-aws-ec2"
   }
 }
