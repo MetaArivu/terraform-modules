ab9a954f114b7ad4347ceb658c52e5e0d8db860a
deepali-arvind <deepali.arvind@metamagic.in>
2021-10-24 10:51:19 +0530

    Delete main.tf

--- Examples/s3/main.tf
+++
@@ -1,8 +0,0 @@
-module "aws-vpc-ariu" {
-  source      = "../../modules/vpc"
-  //awsvpc_cidr = "192.168.0.0/16"
-  ariu_aws_vpc_cidr = var.ariu_aws_vpc_cidr
-  vpc_id      = "${module.aws-vpc-ariu.vpc_id}"
-  //aws_subnet_cidr = "192.168.1.0/24"
-  ariu_aws_subnet_cidr = var.ariu_aws_subnet_cidr
-}
